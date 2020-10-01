354eb4a6199b1f18d148107f3c11145183104989
Leah Mattern <ladyleahmattern@gmail.com>
2020-09-30 20:40:05 -0400

    Setting up the EndBallot Page and the SCSS

--- src/pages/EndBallot/EndBallotPage.js
+++ src/pages/EndBallot/EndBallotPage.js
@@ -1,9 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import './EndBallotPage.scss';
+import logo from "../../images/MyBallotWA.svg";
+import print from "../../images/";
+import email from "../../images/";
+
 
 function EndBallotPage() {
   return (
     <div>
-      <h1>EndBallotPage</h1>
+      <img src={logo} alt="Site logo. myBallotWA" />
+      <p>ELECTION: November 3, 2020</p>
+      <p>Below is your completed ballot</p>
+      <div id="ballot-container">
+
+      </div>
+      <div id="icon-container">
+        <img src={print} alt=""/>
+        <img src={email} alt=""/>
+      </div>
     </div>
   );
 }

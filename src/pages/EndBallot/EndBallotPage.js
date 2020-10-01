14ef1b74172c2efdba1cf28a6978f4704e817256
Leah Mattern <ladyleahmattern@gmail.com>
2020-10-01 00:21:47 -0400

    Final page looks about right. Blue background for the ballot div

--- src/pages/EndBallot/EndBallotPage.js
+++ src/pages/EndBallot/EndBallotPage.js
@@ -4,20 +4,17 @@ import logo from "../../images/MyBallotWA.svg";
 import printthis from "../../images/myballotWA_print.svg";
 import email from "../../images/myballotWA_email.svg";
 
-const ids = ['1'];
-
 function EndBallotPage() {
   return (
     <div className='EndBallotPage'>
-      <img src={logo} alt="Site logo. myBallotWA" />
+      <img id="logo" src={logo} alt="Site logo. myBallotWA" />
       <p>ELECTION: November 3, 2020</p>
       <p>Below is your completed ballot</p>
-        <div id="ballot-container">
-          <p>This is p tag. It has words in it.</p>
-        </div>
       <div id="icon-container">
         <button id="print" ><img src={printthis} alt="Click image to print ballot"/></button>
         <button id="email"><img src={email} alt="Click image to email ballot"/></button>
+      </div>
+      <div id="ballot-container">
       </div>
     </div>
   );

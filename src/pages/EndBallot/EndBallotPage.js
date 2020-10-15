892dce19809f44672e0d98028252a612ab18bd33
Zhijie Xu <jennyxu000@gmail.com>
2020-10-15 12:09:00 -0700

    now

--- src/pages/EndBallot/EndBallotPage.js
+++ src/pages/EndBallot/EndBallotPage.js
@@ -1,14 +1,25 @@
 import React, { useContext } from "react";
 import { DataContext } from "../../App";
 import { Link } from "react-router-dom";
-import Printer, { print } from 'react-pdf-print'
+import { Page, Text, View, Document, StyleSheet } from "@react-pdf/renderer";
 
 import "./EndBallotPage.scss";
 import dklogo from "../../images/myBallotWA_Black.svg";
 import printthis from "../../images/myballotWA_print.svg";
 import email from "../../images/myballotWA_email.svg";
 
-const printIds = ['print1']
+// Create styles
+const styles = StyleSheet.create({
+  page: {
+    flexDirection: "row",
+    backgroundColor: "#E4E4E4",
+  },
+  section: {
+    margin: 10,
+    padding: 10,
+    flexGrow: 1,
+  },
+});
 
 function EndBallotPage() {
   const { ballot, setBallot } = useContext(DataContext);
@@ -24,79 +35,80 @@ function EndBallotPage() {
           <i class="far fa-question-circle"></i>
         </div> */}
       </div>
-
-
-        <div id="print1" >
+      <Document>
+        <Page size="A4" style={styles.page}>
           <img id="logo" src={dklogo} alt="Site logo. myBallotWA" />
           <p>ELECTION: November 3, 2020</p>
           <p>Below is your completed ballot</p>
           <div id="icon-container">
-            <button onClick={() => window.print()} id="print">
+            <button id="print">
               <img src={printthis} alt="Click image to print ballot" />
+            </button>
+            <button id="email">
+              <img src={email} alt="Click image to email ballot" />
             </button>
             <button id="email">
               <img src={email} alt="Click image to email ballot" />
             </button>
           </div>
 
-            <div id="ballot-container">
-              <div className="catCard">
-                <h2>Federal Candidates</h2>
-                {ballot.FederalCandidates.map((candidate) => (
-                  <div className="infoCard">
-                    <p className="position">{candidate.position}</p>
-                    <p className="name">{candidate.name}</p>
-                  </div>
-                ))}
-              </div>
-
-              <div className="catCard">
-                <h2>Statewide Candidates</h2>
-                {ballot.StatewideCandidates.map((candidate) => (
-                  <div className="infoCard">
-                    <p className="position">{candidate.position}</p>
-                    <p className="name">{candidate.name}</p>
-                  </div>
-                ))}
-              </div>
-
-              <div className="catCard">
-                <h2>Judicial Candidates</h2>
-                {ballot.JudicialCandidates.map((candidate) => (
-                  <div className="infoCard">
-                    <p className="position">{candidate.position}</p>
-                    <p className="name">{candidate.name}</p>
-                  </div>
-                ))}
-              </div>
-
-              <div className="catCard">
-                <h2>Legislative Candidates</h2>
-                {ballot.LegislativeCandidates.map((candidate) => (
-                  <div className="infoCard">
-                    <p className="position">{candidate.position}</p>
-                    <p className="name">{candidate.name}</p>
-                  </div>
-                ))}
-              </div>
-
-              <div className="catCard">
-                <h2>Measures</h2>
-                {ballot.Measures.map((measure) => (
-                  <div className="infoCard">
-                    <div className="infoCardBox">
-                      <p className="position">{measure.name}</p>
-                      <p className="name">
-                        {measure.isApproved ? "Approved" : "Rejected"}
-                      </p>
-                    </div>
-                  </div>
-                ))}
-              </div>
+          <div id="ballot-container">
+            <div className="catCard">
+              <h2>Federal Candidates</h2>
+              {ballot.FederalCandidates.map((candidate) => (
+                <div className="infoCard">
+                  <p className="position">{candidate.position}</p>
+                  <p className="name">{candidate.name}</p>
+                </div>
+              ))}
             </div>
-        </div>
 
-      
+            <div className="catCard">
+              <h2>Statewide Candidates</h2>
+              {ballot.StatewideCandidates.map((candidate) => (
+                <div className="infoCard">
+                  <p className="position">{candidate.position}</p>
+                  <p className="name">{candidate.name}</p>
+                </div>
+              ))}
+            </div>
+
+            <div className="catCard">
+              <h2>Judicial Candidates</h2>
+              {ballot.JudicialCandidates.map((candidate) => (
+                <div className="infoCard">
+                  <p className="position">{candidate.position}</p>
+                  <p className="name">{candidate.name}</p>
+                </div>
+              ))}
+            </div>
+
+            <div className="catCard">
+              <h2>Legislative Candidates</h2>
+              {ballot.LegislativeCandidates.map((candidate) => (
+                <div className="infoCard">
+                  <p className="position">{candidate.position}</p>
+                  <p className="name">{candidate.name}</p>
+                </div>
+              ))}
+            </div>
+
+            <div className="catCard">
+              <h2>Measures</h2>
+              {ballot.Measures.map((measure) => (
+                <div className="infoCard">
+                  <div className="infoCardBox">
+                    <p className="position">{measure.name}</p>
+                    <p className="name">
+                      {measure.isApproved ? "Approved" : "Rejected"}
+                    </p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </Page>
+      </Document>
     </div>
   );
 }
